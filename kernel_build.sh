b2d75219c11fc34d3ef4bd0885260bc5c01b6829
Wahyu Studiawan <manusiasampah7@gmail.com>
2022-04-19 21:22:19 +0700

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -17,7 +17,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth=1 https://github.com/Wstudiawan/Parak_Karakah_android_kernel_xiaomi_sdm845 -b new22caf beryllium
+git clone --depth=1 https://github.com/Wstudiawan/kernel_xiaomi_sdm845.git -b pbns_upstream19caf beryllium
 
 git clone --depth=1 https://github.com/Wstudiawan/WS-Clang WS-clang # WS-clang set as Clang Default
 
